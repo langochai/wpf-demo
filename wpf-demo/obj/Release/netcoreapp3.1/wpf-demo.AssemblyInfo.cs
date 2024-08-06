4678d650bebd92a09495b1b17bf5b9b94e812123
La Ngoc Hai <langochai11121998@gmail.com>
2024-08-06 10:27:14 +0700

    1st commit

--- wpf-demo/obj/Release/netcoreapp3.1/wpf-demo.AssemblyInfo.cs
+++ wpf-demo/obj/Release/netcoreapp3.1/wpf-demo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("wpf-demo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0aa14fc385bfa5325b6990918b6c37bf1eea116")]
 [assembly: System.Reflection.AssemblyProductAttribute("wpf-demo")]
